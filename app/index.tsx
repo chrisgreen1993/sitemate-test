0766de511ff7dcfb69bd66acd857176ff46402c1
Chris Green <chrisgreen1993@gmail.com>
2024-06-20 15:20:45 +0700

    Basic news search with newsapi
    Implements fetching news articles for a specific query

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,63 @@
-import { Text, View } from "react-native";
+import React, { useState } from 'react';
+import { FlatList, Keyboard, Linking } from 'react-native';
+import { useQuery } from '@tanstack/react-query';
+import { Searchbar, Text, Button, Card, ActivityIndicator } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Index() {
+const API_KEY = process.env.EXPO_PUBLIC_NEWS_API_KEY
+
+const fetchNews = async (searchTerm: string) => {
+  const res = await fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${API_KEY}`);
+  if (!res.ok) {
+    throw Error('Network response was not ok');
+  }
+  const data = await res.json();
+  return data.articles;
+};
+
+function Index() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const { data: articles, refetch, error, isFetching } = useQuery({
+    queryKey: ['news'],
+    queryFn: () => fetchNews(searchTerm),
+    enabled: false,
+    retry: false
+  });
+
+  const handleSearchOnPress = () => {
+    refetch();
+    Keyboard.dismiss();
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={setSearchTerm}
+        value={searchTerm}
+        onSubmitEditing={() => refetch()}
+      />
+      <Button disabled={!searchTerm.length} mode="contained" onPress={handleSearchOnPress}>Search News</Button>
+      {isFetching && <ActivityIndicator animating />}
+      {error && <Text>Whoops! Looks like something went wrong!</Text>}
+
+      {!isFetching && (
+        <FlatList
+          data={articles}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Card>
+              <Card.Content>
+                <Text variant="titleSmall" onPress={() => Linking.openURL(item.url)}>{item.title}</Text>
+                <Text variant="bodySmall">{item.description}</Text>
+              </Card.Content>
+            </Card>
+          )}
+        />
+      )}
+    </SafeAreaView>
   );
 }
+
+export default Index;
