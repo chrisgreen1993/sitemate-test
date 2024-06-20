72e4ea6958c32cc759faa8b1b0f1a3b62e329fb1
Chris Green <chrisgreen1993@gmail.com>
2024-06-20 15:05:23 +0700

    Add react-native-paper and react-query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from "expo-router";
+import { PaperProvider } from 'react-native-paper';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </PaperProvider>
+    </QueryClientProvider>
   );
 }
