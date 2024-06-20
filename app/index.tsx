2c2b81e5723493a4fe48dfc22ba08e67f31d9851
Chris Green <chrisgreen1993@gmail.com>
2024-06-20 16:24:30 +0700

    Improve spacing and add feedback on touch

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { FlatList, Keyboard, Linking } from 'react-native';
+import { FlatList, Keyboard, Linking, Pressable, StyleSheet, View } from 'react-native';
 import { useQuery } from '@tanstack/react-query';
-import { Searchbar, Text, Button, Card, ActivityIndicator } from 'react-native-paper';
+import { Searchbar, Text, Button, Card, ActivityIndicator, TouchableRipple } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { searchAllNews, Article } from '@/api/newsApi';
 import { useSearchHistory } from '@/hooks/useSearchHistory';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 function Index() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -30,41 +31,63 @@ function Index() {
 
   return (
     <SafeAreaView>
-      <Searchbar
-        placeholder="Search"
-        onChangeText={setSearchTerm}
-        value={searchTerm}
-        onSubmitEditing={() => refetch()}
-      />
-      <Button disabled={!searchTerm.length} mode="contained" onPress={handleSearchOnPress}>Search News</Button>
-      {!!searchHistory.length && (
-        latestSearchHistory.map((item, index) => (
-          <Card key={index} onPress={() => handleSearchHistoryOnPress(item)}>
-            <Card.Content>
-              <Text variant="bodySmall">{item}</Text>
-            </Card.Content>
-          </Card>
-        ))
-      )}
-
-      {isFetching && <ActivityIndicator animating />}
-      {error && <Text>Whoops! Looks like something went wrong!</Text>}
-      {!isFetching && (
-        <FlatList
-          data={articles}
-          keyExtractor={(_item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <Card onPress={() => Linking.openURL(item.url)}>
-              <Card.Content>
-                <Text variant="titleSmall">{item.title}</Text>
-                <Text variant="bodySmall">{item.description}</Text>
-              </Card.Content>
-            </Card>
-          )}
+      <View style={styles.container}>
+        <Searchbar
+          style={styles.defaultMargin}
+          placeholder="Search News"
+          onChangeText={setSearchTerm}
+          value={searchTerm}
+          onSubmitEditing={() => refetch()}
         />
-      )}
+        <Button style={styles.defaultMargin} disabled={!searchTerm.length} mode="contained" onPress={handleSearchOnPress}>Search News</Button>
+        {!!searchHistory.length && (
+          latestSearchHistory.map((item, index) => (
+            <Pressable key={index}>
+              <Card style={styles.defaultMargin} onPress={() => handleSearchHistoryOnPress(item)}>
+                <Card.Content>
+                  <Text variant="bodySmall">{item}</Text>
+                </Card.Content>
+              </Card>
+            </Pressable>
+          ))
+        )}
+
+        {isFetching && <ActivityIndicator style={styles.center} animating />}
+        {error && <Text style={styles.center}>Whoops! Looks like something went wrong!</Text>}
+        {!isFetching && (
+          <FlatList
+            data={articles}
+            keyExtractor={(_item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <Pressable>
+                <Card style={styles.defaultMargin} onPress={() => Linking.openURL(item.url)}>
+                  <Card.Content>
+                    <Text variant="titleSmall">{item.title}</Text>
+                    <Text variant="bodySmall">{item.description}</Text>
+                  </Card.Content>
+                </Card>
+              </Pressable>
+            )}
+          />
+        )}
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    margin: 5,
+  },
+  defaultMargin: {
+    margin: 5
+  }
+});
+
 
 export default Index;
